f4e84bc388a78a7fc5df210aef484aa38b27d5ff
Gustmann <ronald29eduardo@gmail.com>
2024-05-03 14:59:55 -0300

    Projeto concluido

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
-        }
+        Scanner scan = new Scanner(System.in);
+
+        // Solicitando o número da conta
+        System.out.println("Por favor, digite o número da Conta!");
+        int numeroConta = scan.nextInt();
+        scan.nextLine();
+
+        // Solicitando o número da agência
+        System.out.println("Por favor, digite o número da Agência!");
+        String numeroAgencia = scan.nextLine();
+
+        // Solicitando o nome do cliente
+        System.out.println("Por favor, digite seu Nome!");
+        String nomeCliente = scan.nextLine();
+
+        // Solicitando o saldo
+        System.out.println("Por favor, digite seu saldo atual");
+        double saldo = scan.nextDouble();
+        scan.nextLine();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, numeroAgencia, numeroConta, saldo);
+    }
     }
